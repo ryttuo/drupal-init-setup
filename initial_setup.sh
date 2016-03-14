c7395cdd66acde25ba2f0eba0dd38e2354e8284a
andres.solano <andres.solano@gmail.com>
2016-03-14 13:39:23 -0600

    update script

--- initial_setup.sh
+++ initial_setup.sh
@@ -3,18 +3,43 @@
 #the following commands are for start drupal and server configuration over
 #vagrant virtual enviroment
 
-#apache setup
+echo "------------------------------------------------------------------------------"
+echo "--"
+echo "-- Apache and Drupal initial setup"
+echo "--"
+echo "-- @author Andrés Solano S <andres.solano@gmail.com>"
+echo "------------------------------------------------------------------------------"
+echo ""
 
-rm /etc/apache2/sites-enabled/*
-cd /etc/apache2/sites-available/
-cp /home/vagrant/data/sysadmin/apache/drupal.conf 001-drupal.conf
-cd ../sites-enabled/
-ln -s ../sites-available/001-drupal.conf .
-service apache2 restart
+#define dir path
+DIR="~/drupal-init-setup"
 
-#drupal setuo
-cd /home/vagrant/data/instances/news
-drush make news.make -y
-drush site-install minimal --account-name=admin --account-pass=admin --db-url=mysql://root:123456@localhost/news -y
-drush en aggregator contact dashboard field_ui file help image list number options menu path taxonomy -y
-drush en ctools memcache views views_ui features -y
+echo "Cloning provisioning"
+rm -rf ~/drupal-init-setup
+git clone git@github.com:ryttuo/drupal-init-setup.git $DIR
+
+exit
+
+if [ -d $DIR ]; then
+  echo "setup apache and drupal"
+  cd $DIR
+  cp -r data /home/vagrant
+  #rm -r $DIR
+
+  #apache setup
+  rm /etc/apache2/sites-enabled/*
+  cd /etc/apache2/sites-available/
+  cp /home/vagrant/data/sysadmin/apache/drupal.conf 001-drupal.conf
+  cd ../sites-enabled/
+  ln -s ../sites-available/001-drupal.conf .
+  service apache2 restart
+
+  #drupal setuo
+  cd /home/vagrant/data/instances/news
+  drush make news.make -y
+  drush site-install minimal --account-name=admin --account-pass=admin --db-url=mysql://root:123456@localhost/news -y
+  drush en aggregator contact dashboard field_ui file help image list number options menu path taxonomy -y
+  drush en ctools memcache views views_ui features -y
+else
+  echo "Unable to setup the machine. Provision directory not found"
+fi
